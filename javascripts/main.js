6a68bb9aa8fd32932be0fc23c060993e4d02d1bb
Megan B <brown.m.j@outlook.com>
2017-07-08 10:43:45 -0500

    basic functionality before printing to DOM and adding bootstrap

---
+++ javascripts/main.js
@@ -0,0 +1,108 @@
+"use strict";
+
+let $ = require('jquery');
+let categories;
+let types;
+let products;
+let selectedCat;
+let catTypeKeyArr = [];
+let dogTypeKeyArr = [];
+let counter = 0;
+
+let makeProductCard = function() {
+	let domString = "";
+	if (selectedCat === "catFood"){
+		let catCatVal = Object.keys(categories)[0];
+		domString +=
+			`<h1>Cat Food</h1>`;
+		$.each(types, function(key, val) {
+			if (catCatVal === val.category_id) {
+				catTypeKeyArr.push(key);
+			}
+		});
+		$.each(products, function(key, val) {
+				if (counter % 3 === 0) {
+					domString += `<div class="row">`;
+				}
+				if ($.inArray(val.type_id, catTypeKeyArr) !== -1) {
+					domString += `<div class="card" id="${counter}"><h2>${val.name}</h2>`;
+					domString += `<p>${val.description}</p></div>`;
+					counter += 1;
+				}
+				if (counter % 3 === 2) {
+					domString += `</div>`;
+				}
+		});
+		$("#productContainer").append(domString);
+}
+	else {
+		let dogCatVal = Object.keys(categories)[1];
+		$.each(types, function(key, val) {
+			if (dogCatVal === val.category_id) {
+				console.log(val.description);
+				dogTypeKeyArr.push(key);
+			}
+		});
+		$.each(products, function(key, val) {
+			if ($.inArray(val.type_id, dogTypeKeyArr) !== -1) {
+				console.log(val.name);
+			}
+		});
+	}
+};
+
+$("select").change( function() {
+  selectedCat = $(this).val();
+  getCategories()
+  .then( function(dataFromCategories) {
+  	categories = dataFromCategories;
+  	return getTypes();
+  })
+  .then( function(dataFromTypes) {
+  	types = dataFromTypes;
+  	return getProducts(dataFromTypes);
+  })
+  .then( function(dataFromProducts) {
+  	products = dataFromProducts;
+  	makeProductCard();
+  })
+  .catch( function(err) {
+  	console.log("Oops, there was an error:", err.statusText);
+  });
+});
+
+let getCategories = function() {
+	return new Promise ( function(resolve, reject) {
+		$.ajax({
+	  url: "https://acme-explosives-ea213.firebaseio.com/categories.json"
+		})
+		.done( (categories) => {
+		  resolve(categories);
+		})
+		.fail(reject);
+	});
+};
+
+let getTypes = function(dataFromCategories) {
+	return new Promise ( function(resolve, reject) {
+		$.ajax({
+  	url: "https://acme-explosives-ea213.firebaseio.com/types.json"
+		})
+		.done( (types) => {
+		  resolve(types);
+		})
+		.fail(reject);
+	});
+};
+
+let getProducts = function(dataFromCategories, dataFromTypes) {
+	return new Promise ( function(resolve, reject) {
+		$.ajax({
+  	url: "https://acme-explosives-ea213.firebaseio.com/products.json"
+		})
+		.done( (products) => {
+		  resolve(products);
+		})
+		.fail(reject);
+	});
+};
